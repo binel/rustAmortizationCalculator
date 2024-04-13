9e90636a4e0d16f4fefc5191a27404d4ac886ef1
Ben Nelson <binelson13@gmail.com>
2024-04-13 12:10:51 -0500

    Creating amoritization schedule

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::io; 
 
 fn main() {
-    println!("Enter the loan amount:");
-    let loan_amount: f64 = match get_input() {
+    println!("Enter the loan principal:");
+    let principal: f64 = match get_input() {
     	Ok(num) => num,
     	Err(_) => {
     		println!("Please enter a number");
@@ -19,9 +19,31 @@ fn main() {
     	}
     };
     
-    let payment_interest = loan_amount * (interest_rate  / 12.0);
+    println!("Enter the term in years");
+    let term_years: i32 = match get_input() {
+    	Ok(num) => num,
+    	Err(_) => {
+    		println!("Please enter a number");
+    		return;
+    	}
+    };
     
-    println!("The interest on your first payment will be ${:.2}", payment_interest);
+    // Calculate Monthly Payment 
+    let num_payments: i32 = term_years * 12;
+    let monthly_rate: f64 = interest_rate / 12.0;
+    let rp = (1.0 + monthly_rate).powi(num_payments);
+    let monthly_payment: f64 = principal * ((monthly_rate * rp)/(rp - 1.0));
+    
+    
+    println!("Your monthly payment will be ${:.2}", monthly_payment);
+    println!("Schedule:");
+    let mut balance = principal;
+    for i in 0..num_payments {
+    	let interest = balance * monthly_rate;
+    	let principal_payment = monthly_payment - interest;
+    	balance = balance - principal_payment;
+    	println!("Payment {}: Balance: {:.2}, Interest {:.2}, Principal {:.2}", i + 1, balance, interest, principal_payment); 
+    }
 }
 
 fn get_input<T: std::str::FromStr>() -> Result<T, T::Err> {
